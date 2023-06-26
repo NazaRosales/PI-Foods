ce1dace58fd8a57dd9c5c1b60ae9620ce1c86b4b
NazaRosales <nazarenorosales_@hotmail.com>
2023-06-25 21:53:29 -0300

    recipes to global state and api recipes

--- api/src/controllers/getHome.js
+++ api/src/controllers/getHome.js
@@ -5,25 +5,29 @@ const axios = require("axios");
 const getHome = async () => {
   const dbRecipes = await Recipe.findAll();
 
-  /*const { data } = await axios.get(
-    `${URL_API}complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=100`
-  );
-  const { results } = data;
-  let apiRecipes = [];
-  if (results.length) {
-    apiRecipes = results.map((element) => {
-      return {
-        id: element.id,
-        title: element.title,
-        healthScore: element.healthScore,
-        summary: element.summary.replace(/<[^>]+>/g, ""),
-        image: element.image,
-        diet: element.diets ? element.diets : "steps not found",
-      };
-    });
-  }*/
-  console.log(dbRecipes)
-  return dbRecipes; /* [...dbRecipes, ...apiRecipes]; */
+  try {
+    const { data } = await axios.get(
+      `${URL_API}complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=10`
+    );
+    const { results } = data;
+    let apiRecipes = [];
+    if (results.length) {
+      apiRecipes = results.map((element) => {
+        return {
+          id: element.id,
+          title: element.title,
+          healthScore: element.healthScore,
+          summary: element.summary.replace(/<[^>]+>/g, ""),
+          image: element.image,
+          diet: element.diets ? element.diets : "steps not found",
+        };
+      });
+    }
+  } catch (error) {
+    console.log({error: error.message});
+    return dbRecipes; 
+  }
+  return [...dbRecipes, ...apiRecipes];
 };
 
 module.exports = getHome;
