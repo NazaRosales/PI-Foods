a6bc0bcedf2dddb398631fb88ae84ec45fbb507b
NazaRosales <nazarenorosales_@hotmail.com>
2023-06-26 23:15:44 -0300

    starting with filter by name

--- client/src/components/NavBar/navBar.jsx
+++ client/src/components/NavBar/navBar.jsx
@@ -1,6 +1,19 @@
 import { Link } from "react-router-dom";
 import "./navBar.css";
+import sendRequestName from "./sendRequestName";
+import { useState } from "react";
 export default function NavBar() {
+
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleChange = (event) =>{
+    setSearchValue(event.target.value)
+  }
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const res = await sendRequestName(searchValue);
+    console.log(res)
+  }
   return (
     <div className="navBar">
       <Link to="/">
@@ -13,12 +26,18 @@ export default function NavBar() {
       <Link to="/create-recipe">
         <button className="btn">Create Recipe</button>
       </Link>
-      <input
-        className="searchInput"
-        type="text"
-        placeholder="Search: Potato hash with greens..."
-      />
-      <button className="btn">Search Recipe</button>
+      <form 
+      onSubmit={handleSubmit}
+      type="submit">
+        <input
+        value={searchValue}
+        onChange={handleChange}
+          className="searchInput"
+          type="text"
+          placeholder="Search: Potato hash with greens..."
+        />
+        <button className="btn">Search Recipe</button>
+      </form>
     </div>
   );
 }
