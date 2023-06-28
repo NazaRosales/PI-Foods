2c3160645d875232b1cbba497a4c2a9367eea339
NazaRosales <nazarenorosales_@hotmail.com>
2023-06-27 22:59:11 -0300

    select diets

--- client/src/components/filters/filters.jsx
+++ client/src/components/filters/filters.jsx
@@ -1,9 +1,20 @@
+import { useSelector } from "react-redux";
+
 export default function Filters() {
+  const diets = useSelector(state => state.diets);
+
+  const handleSelectChange = (event) => {
+    const selectedDiet = event.target.value;
+    console.log(selectedDiet);
+  };
 
   return (
     <div>
-      <select >
-        <option> Filters </option>
+      <select onChange={handleSelectChange}>
+        <option>All Diets</option>
+        {diets.map(diet => (
+          <option key={diet}>{diet}</option>
+        ))}
       </select>
     </div>
   );
