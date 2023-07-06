a1f34afd7941ba38e78a014e7c361a2f71fa2b79
NazaRosales <nazarenorosales_@hotmail.com>
2023-07-06 17:19:14 -0300

    fixing cards

--- client/src/components/Cards/RecipeCard.jsx
+++ client/src/components/Cards/RecipeCard.jsx
@@ -2,13 +2,20 @@ import "./RecipeCard.css";
 import { Link } from "react-router-dom";
 export default function RecipeCard(props) {
   const { id, image, title, diet } = props;
-  const formatter = new Intl.ListFormat('en',{ style: 'long', type: 'conjunction' })
+  const formatter = new Intl.ListFormat("en", {
+    style: "long",
+    type: "conjunction",
+  });
   return (
-    <Link to = {`/detail/${id}`} className="link">
+    <Link to={`/detail/${id}`} className="link">
       <div className="card">
         <h1 className="cardTitle">{title}</h1>
         <img className="cardImage" src={image} alt={`${title}`} />
-        <p className="cardDescription">Diet: {diet && formatter.format(diet)}.</p>
+        {
+          diet.length 
+          ? (<p className="cardDescription">Diet: {formatter.format(diet)}.</p>) 
+          : (<p className="cardDescription">No related diets.</p>)
+        }
       </div>
     </Link>
   );
